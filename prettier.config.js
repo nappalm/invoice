efa55dc356a8cc9a0fadfd82c82bbfb579f44fbd
Manuel <manuel@thincrs.com>
2022-08-01 19:44:29 -0500

    App configuration

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+};
+
+module.export = options;
